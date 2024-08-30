25a999c6e13e37ae5bb4c308fc43a0c0648b975a
Anton <kiselman.a.v@sevenstones.pro>
2024-08-30 16:21:23 +0700

    swag

--- src/Test.jsx
+++ src/Test.jsx
@@ -16,8 +16,17 @@ import {
     DropdownMenuSeparator,
     DropdownMenuTrigger,
   } from "@/components/ui/dropdown-menu"
+import { useState } from "react";
+
 
 const Test = () => {
+    
+    const [parentHover,setParentHover] = useState(false);
+
+    const handleParentHover=()=>{
+
+    }
+    
     return (
         <>
         <Navbar/>
@@ -105,21 +114,21 @@ const Test = () => {
                 <div className="w-full flex py-8">
                     <input type="text" className="border-b-[1px] border-[#ccc]/25 bg-transparent px-4 text-[#ccc] w-[95%] focus:outline-none" placeholder="Поиск по названию, дате, номеру документа или сделки ...">
                     </input>
-                    <button className="hover:text-white flex border-[1px] rounded-r-sm px-2 py-1 border-[#ccc]/50">
+                    <button className="hover:text-white flex border-[1px] rounded-r-sm px-2 py-1 border-[#ccc]/50 transition"  onMouseEnter={()=>{setParentHover(true)}} onMouseLeave={()=>{setParentHover(false)}}>
                         
-                        <Search/>
+                        <Search color={parentHover?"white":"#ccc"} />
                         Искать
                     </button>
                 </div>
                 <div className="flex flex-wrap justify-between">
                     <div className="basis-1/2">
-                        <p className="underline w-fit">Фильтры</p>
+                        <p className="underline w-fit hover:text-white transition" role="button" >Фильтры</p>
                     </div>
                     <div className="basis-1/2 flex justify-end">
                     <DropdownMenu className="" >
-                    <DropdownMenuTrigger className="text-right w-fit">Сортировка по: Дата добавления</DropdownMenuTrigger>
+                    <DropdownMenuTrigger className="text-right w-fit hover:text-white transition">Сортировка по: Дата добавления</DropdownMenuTrigger>
                         <DropdownMenuContent  className="bg-transparent rounded-sm" side="bottom-right">
-                            <DropdownMenuLabel>Сортировка по</DropdownMenuLabel>
+                            <DropdownMenuLabel className="">Сортировка по</DropdownMenuLabel>
                             <DropdownMenuSeparator />
                             <DropdownMenuItem className="group p-0"><p role="button" className="p-2 w-full text-[#ccc] group-hover:text-black">Дата добавления</p></DropdownMenuItem>
                             <DropdownMenuItem className="group p-0"><p role="button" className="p-2 w-full text-[#ccc] group-hover:text-black">Дата редактирования</p></DropdownMenuItem>
