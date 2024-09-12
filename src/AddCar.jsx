734673cfc2211b464c120078b66edf87332ba8d4
antoshadance <91130807+antoshadance@users.noreply.github.com>
2024-09-12 11:40:29 +0700

    inputs complete

--- src/AddCar.jsx
+++ src/AddCar.jsx
@@ -2,11 +2,12 @@ import Navbar from "./_components/navbar";
 import AdminMenu from "./_components/AdminMenu";
 import "./Test.css"
 import { CircleX, Image, X } from "lucide-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./_components/btnaction.css"
 
 const AddCar = () => {
 
+    
 
     const [formData,setFormData] = useState({
         brand:"",
@@ -32,9 +33,7 @@ const AddCar = () => {
     }
 
     const handleImageDelete = (e) => {
-        let imgSrc = e.target.parentElement.previousElementSibling.attributes.src.value;
-        console.log(previews.find(1))
-        
+            console.log(e.target.parentElement.previousElementSibling)        
     }
 
     const [previews, setPreviews] = useState([])
@@ -53,18 +52,72 @@ const AddCar = () => {
 
     function handleSubmit(e) {
         e.preventDefault();
+               
+            const data = {...formData};
+            console.log(data)
+    
+            fetch("http://localhost:8713/api/cars/create-new",{
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(formData)
+            }).then(setTimeout(()=>{
+                setFormData({
+                    brand:"",
+                    model:"",
+                    year:"",
+                    price:"",
+                    color:"",
+                    engine_capacity:"",
+                    engine_power:"",
+                    mileage:"",
+                    imgs:[],
+                    description: "",
+                })
+                setExtraInputs([])
+                setPreviews([])
+                setNewInputCount(0)
+            },1000))
 
-        const data = {...formData};
-        console.log(data)
-
-        fetch("http://localhost:8713/api/cars/create-new",{
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(formData)
-        })
     }
+
+    const [newInputCount,setNewInputCount] = useState(0);
+    const [extraInputs,setExtraInputs] = useState([]);
+
+    function handleInputNameChange (ev,e,i) {
+        let nextInputs = [...extraInputs]
+        nextInputs[i].name = ev.target.value
+        setExtraInputs(nextInputs)
+    }
+
+    function handleInputValueChange(ev,e,i) {
+        let nextInputs = [...extraInputs]
+        nextInputs[i].value=ev.target.value
+        setExtraInputs(nextInputs)
+    }
+
+    function addNewInput () {
+        setExtraInputs([
+            ...extraInputs,
+            {
+                name: "Новое поле "+newInputCount,
+                value:""
+            }
+        ])
+        setNewInputCount(newInputCount+1)
+    }
+
+    function handleInputDelete (i) {
+        setExtraInputs(extraInputs.filter((e,index)=>i!==index))
+    }
+    
+    useEffect(()=>{
+        setFormData({
+            ...formData,
+            extraInputs:[...extraInputs]
+        })
+    },[extraInputs])
 
     return ( 
         <>
@@ -81,8 +134,8 @@ const AddCar = () => {
 
                         <div className="basis-1/3 h-full px-2 flex flex-col justify-between ">
 
-                            <div className="flex flex-col h-[90%] justify-between container-1 p-1">
-                            <div>
+                            <div className="flex flex-col h-[full] gap-y-5 container-1 p-1">
+                            <div className="">
                             <label htmlFor="brand">Марка автомобиля</label>
                             <input
                             className="bg-[#020a0a]/75 w-full text-[#ccc] border rounded-lg px-2 border-[#ccc]/25"
@@ -138,9 +191,8 @@ const AddCar = () => {
                             </div>
 
                             <div>
-
                             <label >Двигатель</label>
-                            <div className="flex gap-x-2">
+                            <div className="flex gap-x-2 items-end">
 
                                 <div>
                                 <label htmlFor="engine_capacity" >Объем, л.</label>
@@ -164,7 +216,6 @@ const AddCar = () => {
                                 ></input>
                                 </div>
                             </div>
-
                             </div>
 
                             <div>
@@ -178,17 +229,52 @@ const AddCar = () => {
                             ></input>
                             </div>
 
+                            {extraInputs.length!==0&&
+                                extraInputs.map((e,i)=>{
+                                    return (
+                                        <div key={i}>
+                                            <input
+                                            type="text"
+                                            placeholder={e.name}
+                                            className="bg-transparent text-[#ccc] placeholder-[#ccc]/50 focus:outline-none"
+                                            onChange={(ev)=>{handleInputNameChange(ev,e,i)}} 
+                                            />
+                                            <div className="flex">
+                                                <input
+                                                className="bg-[#020a0a]/75 w-full text-[#ccc] border rounded-lg px-2 border-[#ccc]/25"
+                                                placeholder="..."
+                                                name={e.name}
+                                                value={e.value}
+                                                onChange={(ev)=>{handleInputValueChange(ev,e,i)}}
+                                                ></input>
+                                                <button
+                                                type="button"
+                                                className="h-full w-1/6 py-1 flex items-center justify-center"
+                                                onClick={()=>{handleInputDelete(i)}}
+                                                >
+                                                    <X
+                                                    onMouseEnter={handleIconHoverOn}
+                                                    onMouseLeave={handleIconHoverOut}
+                                                    />
+                                                </button>
+                                            </div>
+                                        </div>
+                                    )
+                                })
+                            }
+
                             </div>
 
                             <button className="btn_action bg-transparent border border-white w-full rounded-md p-3 flex items-center justify-center gap-x-4"
                             type="button"
+                            onClick={addNewInput}
                             >
                                 Добавить поле
                             </button>
 
                         </div>
 
-                        <div className="basis-2/3 pl-24 h-full flex flex-col items-center justify-between">
+                        <div className="basis-2/3 pl-24 h-full flex flex-col items-center">
                             
                             <div className="w-full h-full flex flex-col gap-y-16 container-1">
                             <input name="file" id="file" type="file" className="bg-white" height={0} width={0} style={{display: "none"}} multiple onChange={handleImageChange}></input>
@@ -207,14 +293,14 @@ const AddCar = () => {
                                     className="w-full h-full object-cover"
                                     src={previews[0]}
                                     />
-                                    <div className="absolute bottom-0 bg-black/25 backdrop-blur-sm w-full p-2">
-                                        <h2>Основное фото</h2>
+                                    <div className="absolute bottom-0  bg-black/25 backdrop-blur-sm w-full p-2">
+                                        <h2 className="text-white">Основное фото</h2>
                                     </div>
                                     </div>
                                     <div className="container-1 p-1 h-full w-1/2 gap-1 flex flex-wrap justify-between ">
                                         {previews.map((e,i)=>{
                                             return (
-                                                <div className="w-[49%] h-[100px] relative">
+                                                <div className="basis-[49%] h-[100px] relative">
                                                     <img
                                                     key={i}
                                                     src={e}
